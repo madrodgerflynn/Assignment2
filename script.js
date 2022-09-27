4916e20486bb142893ce4f91ab4ca1333cfd5d43
madrodgerflynn <madrodgerflynn@gmail.com>
2022-09-27 00:16:18 -0600

    button works but still no email

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var emailButton = document.getElementById("email-btn");
+const emailButton = document.getElementById("email-btn");
 
 emailButton.addEventListener("click", showEmail);
 
 function showEmail() {
-  console.log("Works");
+  console.log("works");
+  return alert;
 }
